0eb63da1900063565c11c5d9c2b842df3ab4bc0d
Ohjne <er@le.com>
2026-01-04 20:40:09 -0600

    fix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "FriendlyElec Nanopi6 Linux kernel v6.1.118";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs =
     { self, nixpkgs }:
@@ -10,83 +12,46 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system} = {
-        nanopi6Kernel = pkgs.stdenv.mkDerivation {
-          pname = "linux-6.1.118-friendlyelec";
-          version = "6.1.118";
+      packages.${system}.nanopi6Kernel = pkgs.recurseIntoAttrs (
+        pkgs.linuxPackagesFor (
+          pkgs.buildLinux {
+            inherit (pkgs) stdenv;
 
-          src = pkgs.fetchFromGitHub {
-            owner = "friendlyarm";
-            repo = "kernel-rockchip";
-            rev = "ebb487a1ce6e8970638a243b27eb95b9adc9ece1";
-            hash = "sha256-1/z3J1UaEKrOVaI3Kq3tV2ZrBhBDmWcqIvfWHpZO57o=";
-          };
+            version = "6.1.118-friendlyelec";
+            modDirVersion = "6.1.118";
 
-          r8125Src = pkgs.fetchFromGitHub {
-            owner = "friendlyarm";
-            repo = "r8125";
-            rev = "c4cb053bff438fac317e2371cce45985098e4c49";
-            hash = "sha256-ZX/BsuxQONH4nyHZNnE9oqcQJdzfc+1ollpf8wOJ5Qc=";
-          };
+            src = pkgs.fetchFromGitHub {
+              owner = "friendlyarm";
+              repo = "kernel-rockchip";
+              rev = "ebb487a1ce6e8970638a243b27eb95b9adc9ece1";
+              hash = "sha256‑1/z3J1UaEKrOVaI3Kq3tV2ZrBhBDmWcqIvfWHpZO57o=";
+            };
 
-          nativeBuildInputs = [
-            pkgs.makeWrapper
-            pkgs.patchelf
-          ];
+            kernelPatches = [
 
-          buildPhase = ''
-            # Go to the kernel source
-            cd ''${src}
+              # Optional: additional config you need
+              {
+                name = "nanopi6 nanopi6 defconfig";
+                patch = null;
+                extraConfig = ''
+                  CONFIG_LOCALVERSION="-friendlyelec"
+                '';
+              }
 
-            # force nanopi6 config and build
-            make ARCH=arm64 nanopi6_linux_defconfig
-            make ARCH=arm64 nanopi6-images -j$(nproc)
+            ];
 
-            # modules
-            mkdir -p out-modules && rm -rf out-modules/*
-            make ARCH=arm64 INSTALL_MOD_PATH="$PWD/out-modules" modules -j$(nproc)
-            make ARCH=arm64 INSTALL_MOD_PATH="$PWD/out-modules" modules_install INSTALL_MOD_STRIP=1
+            extraConfig = ''
+              # additional config if needed
+            '';
 
-            # get the kernel version in shell
-            KERNEL_VER=$(make ARCH=arm64 kernelrelease)
+            # modules must be built and installed
+            autoModules = true;
+            ignoreConfigErrors = true;
+          }
+        )
+      );
 
-            # build r8125
-            export ETHTOOL_LEGACY_2500baseX=y
-            cp -r ''${r8125Src} r8125
-            cd r8125
-            make ARCH=arm64 KSRC=../ CONFIG_VENDOR_FRIENDLYARM=y CONFIG_WERROR=n -j$(nproc)
-            strip --strip-unneeded r8125.ko
-            mkdir -p ../out-modules/lib/modules/''${KERNEL_VER}/extra/
-            cp r8125.ko ../out-modules/lib/modules/''${KERNEL_VER}/extra/
-            cd ..
-            unset ETHTOOL_LEGACY_2500baseX
-
-            # depmod
-            [ ! -f "$PWD/out-modules/lib/modules/''${KERNEL_VER}/modules.dep" ] && \
-              depmod -b $PWD/out-modules -E Module.symvers -F System.map -w ''${KERNEL_VER}
-
-            # ensure kernel image is in out
-            mkdir -p out
-            cp arch/arm64/boot/Image out/Image
-          '';
-
-          installPhase = ''
-            mkdir -p $out/lib/modules
-            cp -r out-modules/lib/modules/* $out/lib/modules/
-
-            mkdir -p $out/kernel
-            cp out/Image $out/kernel/Image
-          '';
-
-          meta = with pkgs.lib; {
-            description = "FriendlyElec Nanopi6 Linux kernel v6.1.118";
-            license = licenses.gpl3;
-            platforms = [ "aarch64-linux" ];
-          };
-        };
-      };
-
-      nixosModules.nanopi6Kernel =
+      nixosModules.nanopi6KernelConfig =
         { }:
         {
           boot.kernelPackages = self.packages.${system}.nanopi6Kernel;
